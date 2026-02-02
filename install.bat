ff2b8b294f516430327dd70001f6251fda840a08
Krzysztof Tyszko <krzysztof_tyszko@outlook.com>
2026-02-02 19:43:23 +0100

    add extra parameter to batch script

--- install.bat
+++ install.bat
@@ -1,11 +1,16 @@
 @ECHO OFF
 
 if "%1"=="" (
+    echo "Missing parameter with disk name"
+    exit /b 1
+)
+
+if "%2"=="" (
     echo "Missing parameter with user name"
     exit /b 1
 )
 
-set "pluginPath=C:\Users\%1\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins"
+set "pluginPath=%1\Users\%2\AppData\Roaming\QGIS\QGIS3\profiles\default\python\plugins"
 
 if exist "%pluginPath%" (
     rmdir /s /q "%pluginPath%"
